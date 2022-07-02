c13dda721d82e4f81cdedaed048e27388e15f49e
Mohammed075 <94587512+Mohammed075@users.noreply.github.com>
2022-07-02 06:40:34 +0000

    8

--- ActivitySet01/problem08.py
+++ ActivitySet01/problem08.py
@@ -1,3 +1,21 @@
 # Files
 
-filename = "dataset/mbox-short.txt"
+fname = "dataset/romeo.txt" 
+  
+  
+ fname = input("Enter filename: ") 
+ fhand = open(fname) 
+ lst = list() 
+ for line in fhand: 
+     l= line.split() 
+     for i in l: 
+         if i in lst: 
+             continue 
+         else: 
+             lst.append(i) 
+ lst.sort()             
+ print(lst) 
+  
+ #for i in l: 
+ #if i not in lst: 
+      #lst.append(i)
